2489ed1fc1aa028e2bdf989fe52d3cf4a1affac1
Qazale_Tlbn <ghazal7988@gmail.com>
2023-11-16 18:33:10 +0330

    Initial commit

---
+++ todo.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("taskInput");
+    const addTaskButton = document.getElementById("addTask");
+    const searchInput = document.getElementById("searchInput");
+    const searchTaskButton = document.getElementById("searchTask");
+    const taskList = document.getElementById("taskList");
+    const totalTasks = document.getElementById("totalTasks");
+    const completedTasks = document.getElementById("completedTasks");
+    const remainingTasks = document.getElementById("remainingTasks");
+
+
+   
+});
